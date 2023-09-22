3753ee00e24693acaca9807e8b6b16d0530c24ab
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-09-22 10:49:53 -0300

    feat: button options

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,9 +1,30 @@
-import { CheckSquare, Square } from 'phosphor-react'
+import { CheckSquare, CircleNotch, List, Square } from 'phosphor-react'
 import styles from './Box.module.css'
 import { useState } from 'react'
 
 export function Box() {
     const [buttonType, setButtonType] = useState('primary')
+    const [disabled, setDisabled] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [movable, setMovable] = useState(false)
+
+    const setLoadingOption = (value) => {
+        if (value) {
+            setLoading(true)
+            setMovable(false)
+        } else {
+            setLoading(false)
+        }
+    }
+
+    const setMovableOption = (value) => {
+        if (value) {
+            setLoading(false)
+            setMovable(true)
+        } else {
+            setMovable(false)
+        }
+    }
 
     return (
         <article className={styles.container}>
@@ -14,19 +35,26 @@ export function Box() {
             </header>
             <div className={styles.content}>
                 <div className={styles.example}>
-                    <button className={[styles[buttonType]]}>INTERAJA COMIGO</button>
+                    <button disabled={disabled} className={`${styles[buttonType]} ${loading && styles.buttonLoading} ${movable && styles.buttonMovable}`}>
+                        {loading && <CircleNotch className={styles.loading} size={18} />}
+                        {movable && <List size={18} />}
+                        INTERAJA COMIGO
+                    </button>
                 </div>
                 <div className={styles.options}>
                     <div>
-                        <Square size={17} />
+                        {!disabled && <Square size={17} onClick={() => setDisabled(true)} />}
+                        {disabled && <CheckSquare size={17} onClick={() => setDisabled(false)} />}
                         <span>DISABLED</span>
                     </div>
                     <div>
-                        <Square size={17} />
+                        {!loading && <Square size={17} onClick={() => setLoadingOption(true)} />}
+                        {loading && <CheckSquare size={17} onClick={() => setLoadingOption(false)} />}
                         <span>LOADING</span>
                     </div>
                     <div>
-                        <Square size={17} />
+                        {!movable && <Square size={17} onClick={() => setMovableOption(true)} />}
+                        {movable && <CheckSquare size={17} onClick={() => setMovableOption(false)} />}
                         <span>MOVABLE</span>
                     </div>
                 </div>
